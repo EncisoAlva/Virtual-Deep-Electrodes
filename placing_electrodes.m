7cdc62ea288494077e4c15965e6a3541c4fad803
Julio C Enciso-Alva <enciso.alva.jc@gmail.com>
2022-11-27 19:04:59 -0600

    Rename and finishing
    Right now it is an usable extension of what I used on the project.
    
    The current example uses the upper pial (cover of brain), but I plan instead using an inflated cortex.

---
+++ placing_electrodes.m
@@ -0,0 +1,210 @@
+%% open brainstorm
+%
+%addpath 'D:\brainstorm3'
+%brainstorm
+
+%%
+% PARAMETERS
+
+%cortex_vert = piall;
+%cortex_vert = cortex_vert(cortex_vert(:,3)>0.01,:);
+
+dev = true;
+vis_width = 0.001;
+vis_tol = vis_width/20;
+max_iter = 100;
+
+% strip of AxB electrodes is --for now-- assumed parallel to medial line
+% AxB means each B electrode line is perpendicular
+ElecGridNum = [1,4];
+ElecGridSep = [1,0.01];
+ElectRefStart = [0.01,0.01];
+ElectSide = 'left';
+
+if(dev)
+    % DELETE
+    figure()
+    scatter3(cortex_vert(:,1),cortex_vert(:,2),cortex_vert(:,3),'filled')
+    title('Only cortex surface (mesh vertices)')
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+end
+
+%%
+% IDENTIFICATION OF ANTERIOR EDGE
+
+% finding anterior terminal
+% 1. find max
+% 2. select pts close to the max
+% 3. find quasi-max (~100th percentile)
+% 4. repeat 2,3 until convergence
+[M,~] = max( cortex_vert(:,1) );
+old_M = M+vis_tol*2;
+iter = 0;
+%M_col = [M];
+while ( abs(M-old_M) >= vis_tol ) && ( iter < max_iter )
+    strip_idx = abs( cortex_vert(:,1)-M ) < vis_width;
+    old_M = M;
+    M = quantile(cortex_vert(strip_idx,1), 0.98);
+    %M_col = [M_col, M];
+    iter = iter +1;
+end
+if dev
+    % DELETE LATER
+    figure()
+    scatter(cortex_vert(:,1),cortex_vert(:,3),'filled')
+    xlabel('x')
+    ylabel('z')
+    title('Only cortex surface (mesh vertices)')
+    subtitle('Sagittal view')
+    hold on 
+    xline(M,'-','Anterior border','LineWidth',1)
+end
+
+% finding the anterior edge
+% 1. find most superior point in anterior terminal
+% 2. use the xz coordinates as anterior edge
+strip_idx = abs( cortex_vert(:,1)-M ) < vis_width;
+tmp_vec = cortex_vert(strip_idx,:);
+m = quantile( tmp_vec(:,3)-abs(tmp_vec(:,1)-M), 0.98 );
+[~,m_idx] = min( abs(tmp_vec(:,1)-M)+abs(tmp_vec(:,3)-m) );
+AntEdge_x = tmp_vec(m_idx,1);
+AntEdge_z = tmp_vec(m_idx,3);
+if dev
+    figure()
+    scatter(cortex_vert(:,1),cortex_vert(:,3),'filled')
+    xlabel('x')
+    ylabel('z')
+    title('Cortex Surface (Mesh Vertices)')
+    subtitle('Sagittal View')
+    hold on 
+    scatter(AntEdge_x,AntEdge_z,200,'k','filled')
+    xline(M,'--','LineWidth',1)
+    legend('','Anterior Edge','Anterior Extreme')
+
+    vis_idx = vecnorm( cortex_vert(:,[1,3])-[AntEdge_x,AntEdge_z], 2, 2 ) < 2*vis_width;
+    figure()
+    scatter3(cortex_vert(:,1),cortex_vert(:,2),cortex_vert(:,3),'filled')
+    xlabel('x')
+    ylabel('y')
+    zlabel('z')
+    title('Cortex Surface (Mesh Vertices)')
+    hold on 
+    scatter3(cortex_vert(vis_idx,1),cortex_vert(vis_idx,2),cortex_vert(vis_idx,3),'r','filled')
+    legend('','Anterior Edge')
+end
+
+%%
+
+% draw the AxB grid electrodes algorithmicalli
+% 1. draw medial line, for reference
+% 2. mark B regular dots in medial line
+% 3. draw lines parallel to medial line
+% 4. mark A regular dots on each one
+% 5. TODO correction for robustness
+
+% 1.
+med_line = cortex_vert( abs(cortex_vert(:,2))<vis_width, :);
+med_line = med_line( med_line(:,3)>AntEdge_z, :); % only upper part is relevant
+if dev
+    % DELETE
+    figure()
+    scatter3(cortex_vert(:,1),cortex_vert(:,2),cortex_vert(:,3),'filled')
+    xlabel('x')
+    ylabel('y')
+    zlabel('y')
+    hold on
+    scatter3(med_line(:,1),med_line(:,2),med_line(:,3),'filled')
+end
+
+x_0 = min( med_line(:,1) );
+x_f = max( med_line(:,1) );
+x_lo = x_0:0.0001:x_f; 
+y_lo = x_lo*0 + 0;
+model = fit( med_line(:,[1,2]), med_line(:,3), 'lowess' );
+z_up = feval( model, [x_lo',y_lo'] );
+
+MLcurve = [x_lo', y_lo', z_up]; % medial line, stilyzed curve
+guides_idx = MLcurve;
+
+% 2.
+lin_int = zeros( 1, size(x_lo,2) );
+for ii = 2:size(x_lo,2)
+    lin_int(ii) = lin_int(ii-1) + norm( MLcurve(ii,:)-MLcurve(ii-1,:) );
+end
+lin_int = lin_int(end) - lin_int;
+
+ctr_mark = zeros( ElecGridNum(2) ,3 );
+cursor = ElectRefStart(2);
+for iter = 1:ElecGridNum(2)
+    [~,idx] = min(abs( lin_int-cursor ));
+    ctr_mark(iter,:) = MLcurve(idx,:);
+    cursor = cursor + ElecGridSep(2);
+end
+
+%%%
+
+ElectrodeCoords = zeros( ElecGridNum(1)*ElecGridNum(2), 3 );
+ElectrodePositions = zeros( ElecGridNum(1)*ElecGridNum(2), 2 );
+lazy_counter = 1;
+pos = [1,1];
+
+for iter2 = 1:ElecGridNum(2)
+    % 3.
+    aux_line = cortex_vert( abs(cortex_vert(:,1)-ctr_mark(iter2,1))<vis_width, :);
+    aux_line = aux_line( aux_line(:,3)>AntEdge_z, :); % only upper part is relevant
+    
+    if strcmp( ElectSide, 'left' )
+        y_0 = ctr_mark(iter2,2);
+        y_f = max( aux_line(:,2) );
+    else
+        y_0 = min( aux_line(:,2) );
+        y_f = ctr_mark(iter2,2);
+    end
+    y_lo = y_0:0.0001:y_f; 
+    x_lo = y_lo*0 + ctr_mark(iter2,1);
+    model = fit( aux_line(:,[1,2]), aux_line(:,3), 'lowess' );
+    z_up = feval( model, [x_lo',y_lo'] );
+
+    MLcurve = [x_lo', y_lo', z_up]; % medial line, stilyzed curve
+    guides_idx = [guides_idx; MLcurve];
+
+    % 4.
+    lin_int = zeros( 1, size(x_lo,2) );
+    for ii = 2:size(x_lo,2)
+        lin_int(ii) = lin_int(ii-1) + norm( MLcurve(ii,:)-MLcurve(ii-1,:) );
+    end
+    %lin_int = lin_int(end) - lin_int;
+
+    cursor = ElectRefStart(1);
+    pos(2) = 1;
+    for iter = 1:ElecGridNum(1)
+        [~,idx] = min(abs( lin_int-cursor ));
+        ElectrodeCoords(lazy_counter,:) = MLcurve(idx,:);
+        ElectrodePositions(lazy_counter,:) = pos;
+        cursor = cursor + ElecGridSep(1);
+        lazy_counter = lazy_counter+1;
+        pos(2) = pos(2)+1;
+    end
+    pos(1) = pos(1)+1;
+end
+
+if(dev)
+    figure()
+    scatter3(cortex_vert(:,1),cortex_vert(:,2),cortex_vert(:,3),'filled')
+    xlabel('x')
+    ylabel('y')
+    zlabel('y')
+    title('Cortex surface')
+    hold on
+    %scatter3(ctr_mark(:,1),ctr_mark(:,2),ctr_mark(:,3),200,'filled')
+    scatter3(guides_idx(:,1),guides_idx(:,2),guides_idx(:,3),'filled')
+    scatter3(ElectrodeCoords(:,1),ElectrodeCoords(:,2),ElectrodeCoords(:,3),200,'filled')
+    legend('','Reference lines','Electrodes')
+end
+
+%%
+% labels
+
+writematrix(ElectrodeCoords)
